71e744c3934715dd23fddb0531b1570acfb0106a
Nikhil <nikhilrajanand321@gmail.com>
2026-08-27 11:55:58 +0530

    Add Hello World using object

---
+++ HelloUsingObject.java
@@ -0,0 +1,9 @@
+public class HelloUsingObject {
+    void display(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args){
+        HelloUsingObject obj = new HelloUsingObject();
+        obj.display();
+    }
+}
